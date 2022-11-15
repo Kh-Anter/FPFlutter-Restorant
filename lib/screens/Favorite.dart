1123efda5e7d73a538cc563195b93d49e381e7cd
Ahmed Khalaf <ahmedkhalaf22696@gmail.com>
2022-11-15 11:15:57 +0200

    itemdetails

--- lib/screens/Favorite.dart
+++ lib/screens/Favorite.dart
@@ -31,95 +31,105 @@ class _FavoritePageState extends State<FavoritePage> {
               padding: const EdgeInsets.all(10.0),
               child: Column(
                 children: [
+                  Center(
+                      child: Text(
+                        'Swipe on an Item To delete',
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey[700],
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
                   Expanded(
                     child: ListView.separated(
                       physics: BouncingScrollPhysics(),
                       itemBuilder: (context, index) => Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: (){
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) =>  itemDetails(favcontroller.favoritelist[index].name,
-                              favcontroller.favoritelist[index].price,favcontroller.favoritelist[index].description,favcontroller.favoritelist[index].pic,
-                            ),)
-                            );
-
+                        child: Dismissible(
+                          onDismissed: (ind){
+                            favcontroller.deletefavitem(favcontroller.favoritelist[index].itemid,);
                           },
-                          child: Container(
-                            width: double.maxFinite,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2
-                              )
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                    MainAxisAlignment.start,
-                                    children: [
-                                      CircleAvatar(
-                                        radius: 40,
-                                        backgroundImage: AssetImage(
-                                            'assets/images/home/meal.jpg'),
-                                      ),
-                                      SizedBox(
-                                        width: 20,
-                                      ),
-                                      Column(
-                                        crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            favcontroller.favoritelist[index].name,
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 16),
-                                          ),
-                                          SizedBox(
-                                            height: 10,
-                                          ),
-                                          Text(
-                                            favcontroller
-                                                .favoritelist[index].price,
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 16,
-                                                color: Colors.orange),
-                                          ),
-                                          SizedBox(
-                                            height: 10,
-                                          ),
-                                          Container(
-                                            width: 200,
-                                            child: Text(
-                                              favcontroller.favoritelist[index].description,
+                          key: Key('item ${index}'),
+                          background: Container(child: Icon(Icons.delete)),
+                          child: GestureDetector(
+                            onTap: (){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) =>  itemDetails(favcontroller.favoritelist[index].name,
+                                favcontroller.favoritelist[index].price,favcontroller.favoritelist[index].description,favcontroller.favoritelist[index].pic,
+                              ),)
+                              );
+
+                            },
+                            child: Container(
+                              width: double.maxFinite,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2
+                                )
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                      MainAxisAlignment.start,
+                                      children: [
+                                        CircleAvatar(
+                                          radius: 40,
+                                          backgroundImage: AssetImage(
+                                              'assets/images/home/meal.jpg'),
+                                        ),
+                                        SizedBox(
+                                          width: 20,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              favcontroller.favoritelist[index].name,
                                               style: TextStyle(
                                                   fontWeight: FontWeight.bold,
                                                   fontSize: 16),
-                                              maxLines: 1,
-                                              overflow: TextOverflow.ellipsis,
-                                              softWrap: false,
                                             ),
-                                          ),
-                                        ],
-                                      ),
-                                      Spacer(),
-                                      IconButton(onPressed: (){
-                                        setState(() {
-                                          favcontroller.deletefavitem(favcontroller.favoritelist[index].itemid,);
-                                          favcontroller.getfavitem();
-                                        });
-
-                                      }, icon: Icon(Icons.delete)),
-                                    ],
-                                  )
-                                ],
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text(
+                                              favcontroller
+                                                  .favoritelist[index].price,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 16,
+                                                  color: Colors.orange),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Container(
+                                              width: 200,
+                                              child: Text(
+                                                favcontroller.favoritelist[index].description,
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 16),
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                                softWrap: false,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ),
